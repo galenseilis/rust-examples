779ec84dcd203ccee6c1000fddbe5938955cf586
galenseilis <galen.seilis@seilis.ca>
2023-12-28 12:05:28 -0800

    More introduction features

---
+++ a-gentle-introduction-to-rust/1_adding_smart.rs
@@ -0,0 +1,5 @@
+fn main() {
+	let n = 4;
+	let sum = n * (n + 1) / 2;
+	println!("sum is {}", sum)
+}
---
+++ a-gentle-introduction-to-rust/add2.rs
@@ -0,0 +1,7 @@
+fn main() {
+	let mut sum = 0u32;
+	for i in 0..5 {
+		sum += i;
+	}
+	println!("sum is {}", sum);
+}
