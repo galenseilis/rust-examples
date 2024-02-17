40c7e873dc3eb11e455088d2787165c1f2089895
galenseilis <galen.seilis@seilis.ca>
2024-02-17 15:09:01 -0800

    float and numerical ops

---
+++ the-rust-programming-language/data-types/floating-point-types/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+	let x = 2.0; // default type is f64
+	let y: f32 = 3.0; // Explicitly f32
+}
---
+++ the-rust-programming-language/data-types/numeric-operations/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+	// Addition
+	let sum = 5 + 10;
+
+	 // Subtraction
+	let difference = 95.5 - 4.3;
+
+	// Multiplication
+	let product = 4 * 30;
+
+	// Division
+	let quotient = 56.7 / 32.2;
+	let truncated = -5 / 3; // Results in -1
+
+	// Remainder
+	let remainder = 43 % 5;
+}
