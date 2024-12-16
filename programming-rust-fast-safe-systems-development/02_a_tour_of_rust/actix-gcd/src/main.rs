830829635ae594f0e7505a223b4bcd8ebcce4755
galenseilis <galen.seilis@seilis.ca>
2024-12-15 22:02:56 -0800

    actix-web example; working now!

--- programming-rust-fast-safe-systems-development/02_a_tour_of_rust/actix-gcd/src/main.rs
+++ programming-rust-fast-safe-systems-development/02_a_tour_of_rust/actix-gcd/src/main.rs
@@ -1,9 +1,12 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
-fn main() {
+#[actix_web::main]
+async fn main() {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Serving on http://localhost:3000...");
@@ -11,10 +14,12 @@ fn main() {
     server
         .bind("127.0.0.1:3000")
         .expect("error binding server to address")
-        .run();
+        .run()
+        .await
+        .expect("error running server");
 }
 
-fn get_index() -> HttpResponse {
+async fn get_index() -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/html")
         .body(
@@ -27,4 +32,40 @@ fn get_index() -> HttpResponse {
                 </form>
             "#,
         )
+}
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response = 
+        format!("The greatest common divisor of the numbers {} and {} \
+                 is <b>{}</b>\n",
+                form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
 }
