5eb35041d64b3fb5910188aa1d2a3b3a649cc811
galenseilis <galen.seilis@seilis.ca>
2025-04-19 07:20:47 -0700

    defining an enum

--- the-rust-programming-language/6-enums-and-pattern-matching/defining-an-enum/src/main.rs
+++ the-rust-programming-language/6-enums-and-pattern-matching/defining-an-enum/src/main.rs
@@ -12,9 +12,36 @@ fn main() {
     {
         enum Message {
             Quit,
-            Move { x: i32, y: i32},
+            Move { x: i32, y: i32 },
             Write(String),
             ChangeColor(i32, i32, i32),
         }
+    }
+
+    {
+        struct QuitMessage; // unit struct
+        struct MoveMessage {
+            x: i32,
+            y: i32,
+        }
+        struct WriteMessage(String); // tuple struct
+        struct ChangeColorMessage(i32, i32, i32); // also a tuple struct
+    }
+    {
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+            ChangeColor(i32, i32, i32),
+        }
+
+        impl Message {
+            fn call(&self) {
+                // method both would be defined here
+            }
+        }
+
+        let m = Message::Write(String::from("hello"));
+        m.call()
     }
 }
