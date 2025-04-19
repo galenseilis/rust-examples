93c2efafd907dbaaefa67b7aaba88aad8ad4919e
galenseilis <galen.seilis@seilis.ca>
2025-04-19 07:30:08 -0700

    enum stuff

---
+++ the-rust-programming-language/6-enums-and-pattern-matching/the-option-enum-and-its-advantages-over-null-values/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    {
+        enum Option<T> {
+            None,
+            Some(T),
+        }
+    }
+    {
+        let some_number = Some(5);
+        let some_char = Some('e');
+        let absent_number: Option<i32> = None;
+    }
+}
