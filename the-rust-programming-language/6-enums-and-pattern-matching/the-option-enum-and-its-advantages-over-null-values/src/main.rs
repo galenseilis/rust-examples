18091a03bfe00af3cf21b71aca8b010dd402f5a0
galenseilis <galen.seilis@seilis.ca>
2025-05-18 20:14:00 -0700

    ...

--- the-rust-programming-language/6-enums-and-pattern-matching/the-option-enum-and-its-advantages-over-null-values/src/main.rs
+++ the-rust-programming-language/6-enums-and-pattern-matching/the-option-enum-and-its-advantages-over-null-values/src/main.rs
@@ -6,8 +6,13 @@ fn main() {
         }
     }
     {
-        let some_number = Some(5);
-        let some_char = Some('e');
+        let some_number: Option<u8> = Some(5);
+        let some_char: Option<char> = Some('e');
         let absent_number: Option<i32> = None;
+    }
+    {
+        let x: i8 = 5;
+        let y: Option<i8> = Some(5);
+        let sum = x + y;
     }
 }
